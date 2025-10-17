65f2cbc30aceb5509f1373424e1b4f69e7cda9a8
IsApps <96055972+IsApps@users.noreply.github.com>
2025-10-17 11:40:49 -0400

    Create CountdownTimer.java

---
+++ CountdownTimer.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Count down from 10 to 0
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Blast Off!");
+    }
+}

7cf68dcd3eba840454ad6d4ec7b6272cdfb673cc
IsApps <96055972+IsApps@users.noreply.github.com>
2025-10-23 09:30:57 -0400

    Create FactorialCalculator.java

---
+++ FactorialCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Ask the user for a number
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+
+        int factorial = 1; // Variable to store the result
+
+        // Multiply number

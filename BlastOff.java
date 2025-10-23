3b8f54a089aab9bd7f5e3e29e42282152d96e6bc
IsApps <96055972+IsApps@users.noreply.github.com>
2025-10-23 09:30:17 -0400

    Create BlastOff.java

---
+++ BlastOff.java
@@ -0,0 +1,11 @@
+public class CountdownTimer {
+    public static void main(String[] args) {
+        // Loop from 10 down to 0
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i); // Print the current number
+        }
+
+        // After the loop ends, print the final message
+        System.out.println("Blast Off!");
+    }
+}
